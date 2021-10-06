961b49397782d2f009c3b833014e81fbac53adcc
Zusyaku <71582475+Zusyaku@users.noreply.github.com>
2021-10-06 21:42:25 +0700

    Add files via upload

---
+++ dogehero/cfg.php
@@ -0,0 +1,5 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Linux; Android 10; Infinix X692) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.114 Mobile Safari/537.36";
+
+$cookie = "ci_session=86c5d889dbb9e3d52a1df27c09bb914539677351;csrf_cookie_name=265eb20ac6a4b96e2211af630f6fdf6b";
