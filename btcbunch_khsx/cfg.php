079c5b658543803b1b40acc6257ba58b884e2b5c
Zusyaku <71582475+Zusyaku@users.noreply.github.com>
2021-10-11 20:23:58 +0700

    Add files via upload

---
+++ btcbunch_khsx/cfg.php
@@ -0,0 +1,19 @@
+<?php
+//Paste your cookie
+$cookie1 = "";
+$cookie2 = "";
+$cookie3 = "";
+$cookie4 = "";
+$cookie5 = "";
+//Auto witdrawl
+//currencies
+//  BTC=> 1
+//  LTC=> 2
+// DOGE=> 3
+// DASH=> 7
+// USDT=> 9
+// TRX=>  6
+$currencies = ""; //ENTER NUMBER CURRENCIES
+$wallet = "";
+
+?>
