c8b2734c173bfbfd6cf161abb4e5555d18a4a7a4
Zusyaku <71582475+Zusyaku@users.noreply.github.com>
2021-10-12 20:04:05 +0700

    Add files via upload

---
+++ cryptofenz/config.php
@@ -0,0 +1,4 @@
+<?php
+
+//COOKIE DASHBOARD
+$cookie = "ci_session=b5d56982dab1dd5448fda511797bd028857521e2;Referral_Source=android-app%3A%2F%2Forg.telegram.messenger;csrf_cookie_name=185771d1d92ad4ce745671b8b784d849;_ga=GA1.2.2021918397.1634016582;_gid=GA1.2.435123252.1634016582;_gat_gtag_UA_178497691_1=1;__viCookieActive=true;dom3ic8zudi28v8lr6fgphwffqoz0j6c=e57a8822-7947-4850-b6c6-4d6336985c1e%3A1%3A1";
