ed6c7b35706ed42b4432153270d518550030d5d5
Zusyaku <71582475+Zusyaku@users.noreply.github.com>
2021-10-19 19:39:27 +0700

    Add files via upload

---
+++ miningtrx/cfg.php
@@ -0,0 +1,5 @@
+<?php
+
+$useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36";
+
+$cookie = "tronminer_referral=TRSKi8kBFP9Z4RdhyxqQwsWdB9VjnEJuom;PHPSESSID=74tield7k1avpi85o2kc7l7k1u;session_cookie=74tield7k1avpi85o2kc7l7k1u_4237953;session_random=4237953;session_ip=114.124.130.20;session_cntr=ID;ads1350474=1;_ga=GA1.2.1934749771.1634575059;_gid=GA1.2.646314290.1634575059;ads_mellowads=1;cookie_consent_user_accepted=true;tronwallet=TDEDeie45DKPJzqycviWFF8ixWWvtu19nu;cookie_consent_level=%7B%22strictly-necessary%22%3Atrue%2C%22functionality%22%3Atrue%2C%22tracking%22%3Atrue%2C%22targeting%22%3Atrue%7D;_gat_gtag_UA_48165808_5=1";
