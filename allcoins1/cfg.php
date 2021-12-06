2f5ed54b71a74d2234b8e59b9db68d57fb3587a5
Zusyaku <71582475+Zusyaku@users.noreply.github.com>
2021-12-06 20:59:37 +0700

    Add files via upload

---
+++ allcoins1/cfg.php
@@ -0,0 +1,13 @@
+<?php
+
+//Isi USER-AGENT Sesuai Data Kalian
+$user = 'xxxx';
+
+//Isi COOKIE Sesuai Data Kalian
+$cookie = 'xxxx';
+
+//Isi URL-SOLVEMEDIA
+$url_solvemedia = 'https://api-secure.solvemedia.com/papi/_challenge.js?k=CQxN5NMVQDLellX1B00r1vFRb.Ql9aEq;f=_ACPuzzleUtil.callbacks%5B0%5D;l=en;t=img;s=standard;c=js,h5c,h5ct,svg,h5v,v/h264,v/webm,h5a,a/mp3,a/ogg,ua/chrome,ua/chrome77,os/android,os/android11,fwv/BdhRvw.uhxr33,cms/wordpress,jslib/jquery,htmlplus;am=xRS2GeecQC3AzF4B55xALQ;ca=script;ts=1638741466;ct=1638763645;th=white;r=0.06093477652969237';
+
+//Isi Wallet LTC Kalian
+$wallet = "xxxx";
