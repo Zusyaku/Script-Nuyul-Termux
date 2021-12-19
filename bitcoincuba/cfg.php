f5ff5d99111de842b1c15a5c898decb787065be5
Zusyaku <71582475+Zusyaku@users.noreply.github.com>
2021-12-19 09:13:07 +0700

    Add files via upload

---
+++ bitcoincuba/cfg.php
@@ -0,0 +1,9 @@
+<?php
+//ADRESS
+$doge = "DQPhZ8mSJjRqcqKcmdKtfqPdbrPNKAxP5F";
+//Url solve
+$solve_doge = "https://api-secure.solvemedia.com/papi/_challenge.js?k=-bnLELRO3-KT.ueLKwNc9aHbg5NU3N5c;f=_ACPuzzleUtil.callbacks%5B0%5D;l=en;t=img;s=standard;c=js,h5c,h5ct,svg,h5v,v/h264,v/webm,h5a,a/mp3,a/ogg,ua/chrome,ua/chrome96,os/android,os/android10,fwv/Bcg1yg.frfu42,jslib/jquery,htmlplus;am=RUPOBN0qhUdAi.4U3SqFRw;ca=script;ts=1639797167;ct=1639797877;th=white;r=0.2106402626992836";
+// User-agent
+$UserAgent = "Mozilla/5.0 (Linux; Android 10; Infinix X657C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Mobile Safari/537.36";
+
+?>
---
+++ bitcoincuba/install.sh
@@ -0,0 +1,1 @@
+pkg install php -y && pkg install imagemagick -y;pkg update -y && pkg upgrade -y;
