4af8298dfb9efe4fc70eb90fe8f14c033a569c31
Zusyaku <71582475+Zusyaku@users.noreply.github.com>
2021-12-23 22:52:06 +0700

    Add files via upload

---
+++ PARSING/parsing.php
@@ -0,0 +1,45 @@
+<?php
+
+function curl($url, $post = 0, $httpheader = 0, $proxy = 0){ // url, postdata, http headers, proxy, uagent
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_COOKIE,TRUE);
+//        curl_setopt($ch, CURLOPT_COOKIEFILE,"cookie.txt");
+//        curl_setopt($ch, CURLOPT_COOKIEJAR,"cookie.txt");
+        if($post){
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        }
+        if($httpheader){
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $httpheader);
+        }
+        if($proxy){
+            curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
+            curl_setopt($ch, CURLOPT_PROXY, $proxy);
+            // curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+        }
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        $response = curl_exec($ch);
+        $httpcode = curl_getinfo($ch);
+        if(!$httpcode) return "Curl Error : ".curl_error($ch); else{
+            $header = substr($response, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+            $body = substr($response, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+            curl_close($ch);
+            return array($header, $body);
+        }
+    }
+
+
+function get($url){
+  return curl($url,'',head())[1];
+}
+
+function post($url,$data){
+  return curl($url,$data,head())[1];
+}
---
+++ faucetgo1/cfg.php
@@ -0,0 +1,10 @@
+<?php
+
+//Isi USER-AGENT Sesuai Data Kalian
+$user = '';
+
+//Isi Cookie Sesuai Data Kalian
+$cookie = '';
+
+//Isi Url-Solvemedia Sesuai Data Kalian
+$url_solvemedia = 'https://api-secure.solvemedia.com/papi/_challenge.js?k=Pz06uh1ZgbOV1qQZRNaTuP5Pbxg05Vpq;f=_ACPuzzleUtil.callbacks%5B0%5D;l=en;t=img;s=standard;c=js,h5c,h5ct,svg,h5v,v/h264,v/webm,h5a,a/mp3,a/ogg,ua/safari,os/linux,fwv/BbaKMQ.mmif24,htmlplus;am=Baf6tyEnBvEAEW6PIScG8Q;ca=script;ts=1640230484;ct=1640231005;th=white;r=0.6085549084604702';
