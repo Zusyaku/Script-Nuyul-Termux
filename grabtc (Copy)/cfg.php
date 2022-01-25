74d906e1ba616344bccb6e83e95ca2ff8bc2a497
Zusyaku <71582475+Zusyaku@users.noreply.github.com>
2022-01-25 17:57:24 +0700

    Add files via upload

---
+++ grabtc (Copy)/cfg.php
@@ -0,0 +1,10 @@
+<?php
+
+//Isi USER-AGENT Sesuai Data Kalian
+$user = 'xxxx';
+
+//Isi Cookie Sesuai Data Kalian
+$cookie = 'xxxx';
+
+//Isi Url-Solvemedia Sesuai Data Kalian
+$url_solvemedia = 'xxxx';
